769144480d15b94927f4013bfe0a739fa9de4fad
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-04 04:45:21 +0330

    Update GameLife.c - Add ANSI Colors Define

--- GameLife.c
+++ GameLife.c
@@ -11,3 +11,15 @@
 
 
 typedef char World[HEIGHT][WIDTH];
+
+// ANSI color codes
+#define COLOR_RESET "\033[0m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_YELLOW "\033[33m"
+#define COLOR_BLUE "\033[34m"
+#define COLOR_MAGENTA "\033[35m"
+#define COLOR_CYAN "\033[36m"
+#define COLOR_WHITE "\033[37m"
+
+
+
